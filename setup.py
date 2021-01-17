467fac53e91b5041b528f39ee0c861cf025ac763
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-01-17 14:36:39 -0500

    SAD

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="relations",
-    version="0.2.3,
+    version="0.2.3",
     package_dir = {'': 'lib'},
     py_modules = [
         'relations',
