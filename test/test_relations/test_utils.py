1b079ecc58070609bbfbca7bc58fdbdbe184754d
AJ Pryor <apryor6@gmail.com>
2021-01-27 08:20:12 -0500

    Add a better test case

--- test/test_relations/test_utils.py
+++ test/test_relations/test_utils.py
@@ -14,12 +14,24 @@ class Jelly(Model):
     pass
 
 
+class Time(Jelly):
+    pass
+
+
 class TestModelsPresent(unittest.TestCase):
     def test_models_returns_correct_with_class(self):
 
         module = sys.modules[__name__]
         models = relations.models(module)
-        assert len(models) == 2
+        assert len(models) == 3
         assert PeanutButter in models
         assert Jelly in models
+        assert Time in models
 
+    def test_models_from_base_works(self):
+
+        module = sys.modules[__name__]
+        models = relations.models(module, from_base=Jelly)
+        assert len(models) == 1
+        assert Time in models
+        assert Jelly not in models
