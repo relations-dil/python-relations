ac6d3929851f51f094532e2c19631788c6a09927
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-05-31 13:21:13 -0400

    Fixed updates

--- lib/relations/unittest.py
+++ lib/relations/unittest.py
@@ -75,7 +75,8 @@ class MockSource(relations.Source):
         for extracting in [field for field in model._fields._order if field.extract]:
             path = extracting.extract.split('__')
             extracted = model._fields._names[path.pop(0)]
-            values[extracting.store] = extracting.get(values[extracted.store], path)
+            if extracted.store in values:
+                values[extracting.store] = extracting.get(values[extracted.store], path)
 
         return values
 
@@ -258,7 +259,7 @@ class MockSource(relations.Source):
             for updating in model._each("update"):
 
                 values = {}
-                self.record_update(updating._record, values)
+                self.record_update(updating._record, values, changed=True)
 
                 self.data[model.NAME][updating[model._id]].update(self.extract(updating, values))
 
