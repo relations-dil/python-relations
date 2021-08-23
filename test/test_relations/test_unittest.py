17b21a5a432b484a71955dd1e1e91fa8bf03b6c1
Gaffer Fitch <gaffer.fitch@gmail.com>
2021-08-23 10:00:10 -0400

    Beefed up tests of JSON data

--- test/test_relations/test_unittest.py
+++ test/test_relations/test_unittest.py
@@ -546,7 +546,16 @@ class TestSource(unittest.TestCase):
         self.assertEqual(model.name, ["things"])
         self.assertTrue(model.overflow)
 
-        Meta("dive", stuff=[1, 2, 3, None], things={"a": {"b": [1], "c": "sure"}, "4": 5, "for": [{"1": "yep"}]}).create()
+        Meta("dive", people={"tom", "mary"}, stuff=[1, 2, 3, None], things={"a": {"b": [1, 2], "c": "sure"}, "4": 5, "for": [{"1": "yep"}]}).create()
+
+        model = Meta.many(people={"tom", "mary"})
+        self.assertEqual(model[0].name, "dive")
+
+        model = Meta.many(stuff=[1, 2, 3, {"relations.io": {"1": None}}])
+        self.assertEqual(model[0].name, "dive")
+
+        model = Meta.many(things={"a": {"b": [1, 2], "c": "sure"}, "4": 5, "for": [{"1": "yep"}]})
+        self.assertEqual(model[0].name, "dive")
 
         model = Meta.many(stuff__1=2)
         self.assertEqual(model[0].name, "dive")
@@ -573,6 +582,45 @@ class TestSource(unittest.TestCase):
         self.assertEqual(len(model), 0)
 
         model = Meta.many(things___4=6)
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(things___4=6)
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(things__a__b__has=1)
+        self.assertEqual(len(model), 1)
+
+        model = Meta.many(things__a__b__has=3)
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(things__a__b__any=[1, 3])
+        self.assertEqual(len(model), 1)
+
+        model = Meta.many(things__a__b__any=[4, 3])
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(things__a__b__all=[2, 1])
+        self.assertEqual(len(model), 1)
+
+        model = Meta.many(things__a__b__all=[3, 2, 1])
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(people__has="tom")
+        self.assertEqual(len(model), 1)
+
+        model = Meta.many(people__has="dick")
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(people__any=["tom", "dick"])
+        self.assertEqual(len(model), 1)
+
+        model = Meta.many(people__any=["harry", "dick"])
+        self.assertEqual(len(model), 0)
+
+        model = Meta.many(people__all=["mary", "tom"])
+        self.assertEqual(len(model), 1)
+
+        model = Meta.many(people__all=["tom", "dick", "mary"])
         self.assertEqual(len(model), 0)
 
         Net(ip="1.2.3.4", subnet="1.2.3.0/24").create()
